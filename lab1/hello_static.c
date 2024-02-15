026be4350ee114b8ff6ead466dde41e4b7ab7bda
dadashasha <143979250+dadashasha@users.noreply.github.com>
2024-02-16 02:22:49 +0700

    Add files via upload

---
+++ lab1/hello_static.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "hello_static.h"
+
+void hello_from_static_lib(void){
+    printf("Hello, static world!");
+}
+
---
+++ lab1/hello_static.h
@@ -0,0 +1,5 @@
+#ifndef HELLO_STATIC_H
+#define HELLO_STATIC_H
+
+void hello_from_static_lib(void);
+#endif
